ea99981ab5d722e70eddf4f718b7b172fc10e9bc
sujeong.k <45249021+mirro97@users.noreply.github.com>
2022-07-19 23:24:51 +0900

    다른 state 끌어올리기 예제 업로드

--- src/component/LiftingState.js
+++ src/component/LiftingState.js
@@ -1,39 +1,83 @@
 import { useState } from "react";
 
+// 상위 컴포넌트
 function LiftingState() {
-  let [userA, setUserA] = useState(0);
+  let [todo, setTodo] = useState([]);
 
-  let handleMoney = (event) => {
-    let data = event.target.value;
-    setUserA(data);
+  const addTodo = (newTodo) => {
+    setTodo([...todo, newTodo]);
   };
 
   return (
     <div>
-      <h2>환전하기</h2>
-      <input onChange={handleMoney} /> (만원)
-      <br />
-      <MoneyInput type="dollor" money={userA} />
-      <br />
-      <MoneyInput type="euro" money={userA} />
+      <NewMessageForm buttonClick={addTodo} />
+      <ul>
+        {todo.map((item, index) => (
+          <SingleTodo key={index} content={item} />
+        ))}
+      </ul>
     </div>
   );
 }
 
-function toDollar(won) {
-  return won * 0.76;
+// 하위 컴포넌트
+function NewMessageForm({ buttonClick }) {
+  let [message, setMessage] = useState("");
+
+  const inputTodo = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const todoRegist = () => {
+    buttonClick(message);
+  };
+
+  return (
+    <div className="input-container">
+      <input onChange={inputTodo} />
+      <button onClick={todoRegist}>등록하기</button>
+    </div>
+  );
 }
 
-function toEuro(won) {
-  return won * 0.75;
+function SingleTodo(props) {
+  return <li>{props.content}</li>;
 }
 
-function MoneyInput(props) {
-  let result = "";
-  if (props.type === "dollor")
-    return (result = <span>{toDollar(props.money)} (달러)</span>);
-  else if (props.type === "euro");
-  return (result = <span>{toEuro(props.money)} (유로)</span>);
-}
+// function LiftingState() {
+//   let [userA, setUserA] = useState(0);
+
+//   let handleMoney = (event) => {
+//     let data = event.target.value;
+//     setUserA(data);
+//   };
+
+//   return (
+//     <div>
+//       <h2>환전하기</h2>
+//       <input onChange={handleMoney} /> (만원)
+//       <br />
+//       <MoneyInput type="dollor" money={userA} />
+//       <br />
+//       <MoneyInput type="euro" money={userA} />
+//     </div>
+//   );
+// }
+
+// function toDollar(won) {
+//   return won * 0.76;
+// }
+
+// function toEuro(won) {
+//   return won * 0.75;
+// }
+
+// function MoneyInput(props) {
+//   let result = "";
+//   if (props.type === "dollor")
+//     return (result = <span>{toDollar(props.money)} (달러)</span>);
+//   else if (props.type === "euro");
+//   return (result = <span>{toEuro(props.money)} (유로)</span>);
+// }
 
 export { LiftingState };
